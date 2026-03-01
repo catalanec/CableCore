0c164939af1459d4a532d74fdb947d91bf214755
catalanec <strannik07@gmail.com>
2026-03-01 19:14:02 +0100

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -21,6 +21,9 @@ export default function Login() {
   const handleGoogleLogin = async () => {
     await supabase.auth.signInWithOAuth({
       provider: "google",
+      options: {
+        redirectTo: window.location.origin,
+      },
     });
   };
 
